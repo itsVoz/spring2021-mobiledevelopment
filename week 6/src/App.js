9a5e619c86bd7cb3c4a36c399334eec636d85e8e
itsVoz <33042762+itsVoz@users.noreply.github.com>
2021-02-19 14:15:13 -0500

    Week 6 | only Public and src folders

---
+++ week 6/src/App.js
@@ -0,0 +1,15 @@
+// Importing required packages
+import Example from './components/Example/index';
+
+// Creating a new functional component 'App'
+function App() {
+
+    // Returning values to browser
+    // Example class component is being exicuted upon return
+    return(
+      <Example />
+  );
+}
+
+// Exporting default values
+export default App;
---
+++ week 6/src/components/Example/index.js
@@ -0,0 +1,14 @@
+// Importing required packages
+import React from 'react';
+
+// Creating a class component 'Example' that extends React.Component
+class Example extends React.Component {
+
+    // Processing and displaying information to the browser
+    render(){
+        return <p>My name is Matt</p>;
+    }
+}
+
+// Exporting default values
+export default Example;
