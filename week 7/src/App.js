1dab489f097c0226558083958d614939f3be98a9
itsVoz <33042762+itsVoz@users.noreply.github.com>
2021-02-26 11:38:05 -0500

    Week 7 | public and src

---
+++ week 7/src/App.js
@@ -0,0 +1,17 @@
+// Importing required packages
+import Steps from './components/Steps'
+import Laps from './components/Laps'
+
+// Function call to return the dynamic components
+function App() {
+  return (
+    <div >
+      <h1>Exercise Tracker</h1>
+      <Steps/>
+      <Laps/>
+    </div>
+  );
+}
+
+// export default values
+export default App;
---
+++ week 7/src/components/Laps.js
@@ -0,0 +1,23 @@
+// import required packages
+import React, {useState} from 'react';
+
+// Create a function 'Laps'
+// constist of a variable that carries an array
+// [*state variable*, *funtion*] = *initatal values*
+function Laps() {
+    const [lapNumber, setLap] = useState(0)
+
+    // Returns values to browser
+    // Add laps: on click the 'setLap' funtion adds 1 lap to the current lap value
+    // Remove all Laps: setLaps is returned to the value of 0
+    return(
+        <div>
+            <h2>Laps Total: {lapNumber}</h2>
+            <button onClick={() => setLap(lapNumber + 1)}>Add Laps</button>
+            <button onClick={() => setLap(0)}>Remove all Laps</button>
+        </div>
+    )
+}
+
+// Export default values
+export default Laps
